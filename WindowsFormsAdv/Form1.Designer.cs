71d1e8ffd01cddae2740e9ba3df6104bf7450ccc
seonyo <jsy6535@naver.com>
2024-08-22 11:49:16 +0900

    도움말 >  도움말 보기 구현

--- WindowsFormsAdv/Form1.Designer.cs
+++ WindowsFormsAdv/Form1.Designer.cs
@@ -306,6 +306,7 @@
             this.도움말보기ToolStripMenuItem.Name = "도움말보기ToolStripMenuItem";
             this.도움말보기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.도움말보기ToolStripMenuItem.Text = "도움말 보기";
+            this.도움말보기ToolStripMenuItem.Click += new System.EventHandler(this.도움말보기ToolStripMenuItem_Click);
             // 
             // 피드백보내기ToolStripMenuItem
             // 
